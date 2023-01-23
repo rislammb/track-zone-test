8d9edbe0a178286bb47c3c1904ee692547d950ab
Rayhanul Islam <rislammb@gmail.com>
2023-01-23 19:56:07 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
