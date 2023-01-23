208adfb932b82709563171eb8fec4127349c5115
Rayhanul Islam <rislammb@gmail.com>
2023-01-23 22:26:02 +0600

    Create Input.jsx

---
+++ src/ui/Input.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Input = styled.input.attrs(props => ({
+  // we can define static props
+  type: "text",
+
+  // or we can define dynamic ones
+  size: props.size || "1em",
+}))`
+  color: palevioletred;
+  font-size: 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+
+  /* here we use the dynamically computed prop */
+  margin: ${props => props.size};
+  padding: ${props => props.size};
+`;
+
+export default Input; 
