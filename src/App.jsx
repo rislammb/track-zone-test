f6e58b4b7961a0f729fff4ec6ab4a6d35ed04d49
Rayhanul Islam <rislammb@gmail.com>
2023-01-23 21:10:07 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
+import useApp from '../hooks/useApp';
 import "./styles.css";
 
 export default function App() {
+  const { state, addClock, editUserClock, editClock, deleteClock, openModal, closeModal } = useApp();
+ 
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    addClock({ title: 'Test', timeZone: 'GMT', difference: '02:15' })
+  }
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Update magic happen!</h2>
+      <div>
+        <div>
+          <h2>{ state.adminClock.title }</h2>
+          <p>{ state.adminClock.timeZone }</p>
+          <p>{ state.adminClock.difference }</p>
+        </div>
+        <button onClick={openModal}>Add Clock</button>
+      </div>
+
+      { state.clocks.length > 0 && state.clocks.map(clock => (
+        <div>
+          <h2>{ clock.title }</h2>
+          <p>{ clock.timeZone }</p>
+          <p>{ clock.difference }</p>
+        </div>
+      ))}
+
+      { state.open && (
+        <div>
+          <button onClick={closeModal}>Close</button>
+          <form onSubmit={ handleSubmit }>
+            <input />
+            <input />
+            <button>ADD</button>
+          </form>
+        </div>
+      )}    
     </div>
   );
 }
