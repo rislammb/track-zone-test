8e04151ea48986df0e82cecc47f146d1f2282718
Rayhanul Islam <rislammb@gmail.com>
2023-01-24 17:24:14 +0600

    Create Clock.jsx

---
+++ src/components/Clock.jsx
@@ -0,0 +1,27 @@
+import Card from '../ui/Card';
+import Title from '../ui/Title';
+import Text from '../ui/Text';
+import Button from '../ui/Button';
+
+const Clock = ({ adminClock, clock, openModal, deleteClock }) = > {
+  return clock ? (
+    <Card>
+      <Title size={'lg'}>Clock</Title>
+      <Title size={'sm'}>{clock.title}</Title>
+      <Text>{clock.timeZone}({clock.timeZone})</Text>
+
+      <Button color={'warning'} onClick={() = > deleteClock(clock.id)}>Delete</Button>
+      <Button onClick={() = > openModal(clock.id)}>Edit</Button>
+    </Card>
+  ) : (
+    <Card>
+      <Title size={'lg'}>Clock</Title>
+      <Title size={'sm'}>{adminClock.title}</Title>
+      <Text>{adminClock.timeZone}({adminClock.timeZone})</Text>
+
+      <Button onClick={() = > openModal('admin')}>Edit</Button>
+    </Card>
+  )
+}
+
+export default Clock; 
