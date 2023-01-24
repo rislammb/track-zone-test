be6f74486489ee194ff681922bc4131b66317e65
Rayhanul Islam <rislammb@gmail.com>
2023-01-24 10:20:07 +0600

    Update useForm.js

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -4,23 +4,55 @@ const useForm = (initial, validate) => {
   const [state, setState] = useState(mapObjToState(initial));
 
   const handleChange = e => {
-    
+    const { name, value } = e.target;
+    const oldState = JSON.parse(JSON.stringify(state));
+
+    oldState[name].error = '';
+    oldState[name].value = value;
+
+    setState(oldState);
   };
 
   const handleFocus = e => {
-    
+    const { name } = e.target;
+    const oldState = JSON.parse(JSON.stringify(state));
+
+    oldState[name].touched = true;
+    oldState[name].focused = true;
+
+    setState(oldState);
   };
 
   const handleBlur = e => {
-    
+    const { name } = e.target;
+    const oldState = JSON.parse(JSON.stringify(state));
+    oldState[name].focused = false;
+
+    const { errors } = validate(mapStateToValues(oldState));
+
+    if (errors[name] && oldState[name].touched) {
+      oldState[name].error = errors[name];
+    }
+
+    setState(oldState);
   };
 
   const handleSubmit = (e, cb) => {
     e.preventDefault();
-    const { valid, errors } = validate(values) 
-const values = mapStateToValues(state);
 
-    cb(values)
+    const values = mapStateToValues(state);
+    const { valid, errors } = validate(values);
+
+    if (!valid) {
+      const oldState = JSON.parse(JSON.stringify(state));
+
+      Object.keys(errors).forEach(key => {
+        oldState[key].error = errors[key]
+      });
+
+      setState(oldState);
+    }
+    cb(values);
   };
 
   return { state, handleChange, handleFocus, handleBlur, handleSubmit } 
