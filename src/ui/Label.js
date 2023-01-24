337a05fc1ca635040e001bd8f2dd41c2bc149919
Rayhanul Islam <rislammb@gmail.com>
2023-01-24 16:35:08 +0600

    Update Label.js

--- src/ui/Label.js
+++ src/ui/Label.js
@@ -1,10 +1,20 @@
 import styled from 'styled-components';
 
+const sizes = {
+  sm: '0.8em',
+  md: '1em',
+  lg: '1.1em'
+};
+
+const colors = {
+  primary: '#7af',
+  warning: '#fa7'
+};
+
 const Label = styled.label`
-  align-items: center;
-  display: flex;
-  gap: 8px;
-  margin-bottom: 8px;
+  font-size: ${props => props.size ? sizes[props.size] : '1em'};
+  padding: ${props => props.p ?? '4px 0'};
+  color: ${ props => props.color ? colors[props.color] : '#fafafa' };
 `
 
 export default Label; 
