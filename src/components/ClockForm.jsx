d09610c6f706a29dcf6195efc426bf9d962f398a
Rayhanul Islam <rislammb@gmail.com>
2023-01-24 07:28:54 +0600

    Create ClockForm.jsx

---
+++ src/components/ClockForm.jsx
@@ -0,0 +1,58 @@
+import useForm from '../hooks/useForm';
+
+const validate = values => {
+  const errors = {};
+  Object.keys(values).forEach(key => {
+    if (!values[key]) {
+      errors[key] = 'Invalid ' + key; 
+    }
+  })
+
+  return { valid: Object.keys(errors).length === 0, errors  }
+};
+
+const ClockForm = ({ addClock, editAdminClock, editClock, closeModal, openedClock }) => {
+  const initial = openedClock ? {
+    title: openedClock.title,
+    timeZone: openedClock.timeZone,
+    difference: openedClock.difference
+  } : {
+    title: '',
+    timeZone: '',
+    difference: ''
+  };
+
+  const { state, handleChange, handleFocus, handleBlur, handleSubmit } = useForm(initial, validate);
+
+  const submit = (values) => {
+    if (openedClock) {
+      if(openedClok.id){
+        editClock({ ...values });
+      } else{
+        editAdminClock({ ...values });
+      }
+    } else {
+      addClock({ ...values });
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={closeModal}>
+        Close
+      </button>
+
+      <form onSubmit={e => handleSubmit(e, submit)}>
+        <input value={state.title} name='title' onChange={handleChange} />
+        <input value={state.timeZone} name='timeZone' onChange={handleChange} />
+        <input value={state.difference} name='difference' onChange={handleChange} />
+
+        <button type='submit'>
+          { openedClock ? 'Edit Clock' : 'Add Clock' }
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default ClockForm; 
