acf5f62202994ce8084d9fe5ff660c32804f5115
Rayhanul Islam <rislammb@gmail.com>
2023-01-24 16:52:33 +0600

    Update Form.js

--- src/ui/Form.js
+++ src/ui/Form.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
-const Modal = styled.form`
+const Form = styled.form`
   display: flex; 
   flex-direction: column; 
   gap: 8px; 
 `;
 
-export default Modal;
+export default Form;
