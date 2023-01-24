6450f9d6c07500f3e0003010516338893ff6ec11
Rayhanul Islam <rislammb@gmail.com>
2023-01-24 15:36:20 +0600

    Update

--- src/ui/Modal.js
+++ src/ui/Modal.js
@@ -4,6 +4,12 @@ const Modal = styled.div`
   padding: ${props => props.p ? `${props.p * 8}px` : '8px'};
   width: 100vw; 
   height: 100vh; 
+  position: fixed; 
+  top: 0;
+  left: 0;
+  display: flex; 
+  justify-content: center; 
+  align-items: center; 
   z-index: 2;
   background: rgba(255, 255, 255, 0.3);
 `;
