a3265df68f61cc9dd03ea0f7a2cc235b33ce2351
Rayhanul Islam <rislammb@gmail.com>
2023-01-24 15:58:17 +0600

    Update Input.jsx

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -5,15 +5,14 @@ const Input = styled.input.attrs(props => ({
   type: "text",
 
   // or we can define dynamic ones
-  size: props.size || "1em",
+  size: props.size || "4px 8px",
 }))`
   color: palevioletred;
   font-size: 1em;
-  border: 2px solid palevioletred;
+  border: 1px solid palevioletred;
   border-radius: 3px;
 
   /* here we use the dynamically computed prop */
-  margin: ${props => props.size};
   padding: ${props => props.size};
 `;
 
