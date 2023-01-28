926cb87591ca9452dfa35ee6e167fc9247f1a89e
Rayhanul Islam <rislammb@gmail.com>
2023-01-28 20:44:53 +0600

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,14 @@
-export const addZeroFrist = (number) => {
-  return number > 9 ? `${number}` : `0${number}`;
+export const generateId = () => {
+  const v4 = () => {
+    return Math.floor((1 + Math.random()) * 0x1000)
+  .toString(16);
+  }
+  return v4() + v4() + '-' + v4() + '-' + v4() + '-' + v4() + v4();
 };
+
+// export const addZeroFrist = (number) => {
+ // return number > 9 ? `${number}` : `0${number}`;
+// };
 
 export const addMinutesFN = (dateObj, minutes) => {
   return new Date(new Date(dateObj).getTime() + minutes * 60 * 1000);
